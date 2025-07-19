2fdf1400aed35b63a15807a1c1c7046a35f74fc6
Vinicius Cerqueira <viniciusleitecerqueira@gmail.com>
2025-07-19 17:27:18 -0300

    criados metodos put, post, delete

--- src/main/kotlin/com/viniciusleitecerqueira/escola/business/CursoBusiness.kt
+++ src/main/kotlin/com/viniciusleitecerqueira/escola/business/CursoBusiness.kt
@@ -1,11 +1,17 @@
 package com.viniciusleitecerqueira.escola.business
 
-import com.viniciusleitecerqueira.escola.beans.Curso
 import com.viniciusleitecerqueira.escola.repositories.CursoRepository
+import com.viniciusleitecerqueira.escola.repositories.MateriaRepository
+import com.viniciusleitecerqueira.escola.beans.Curso
+import com.viniciusleitecerqueira.escola.beans.Materia
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -25,5 +31,35 @@ class CursoBusiness {
         return if (curso.isPresent) ResponseEntity.ok(curso.get())
         else ResponseEntity.notFound().build()
     }
+
+    @PostMapping
+    fun cadastrar(@RequestBody curso: Curso): Curso =
+        cursoRepository.save(curso)
+
+    @PutMapping("/{id}")
+    fun atualizar(@PathVariable id: Long, @RequestBody atualizado: Curso): ResponseEntity<Curso> {
+        val existente = cursoRepository.findById(id)
+        return if (existente.isPresent) {
+            val curso = existente.get().apply {
+                nome = atualizado.nome
+                cargaHoraria = atualizado.cargaHoraria
+                professorId = atualizado.professorId
+            }
+            ResponseEntity.ok(cursoRepository.save(curso))
+        } else {
+            ResponseEntity.notFound().build()
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    fun deletar(@PathVariable id: Long): ResponseEntity<Void> {
+        return if (cursoRepository.existsById(id)) {
+            cursoRepository.deleteById(id)
+            ResponseEntity.noContent().build()
+        } else {
+            ResponseEntity.notFound().build()
+        }
+    }
+
 
 }
