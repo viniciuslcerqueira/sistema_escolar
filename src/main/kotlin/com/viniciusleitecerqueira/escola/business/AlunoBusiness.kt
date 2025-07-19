deea2bd1a6fdb27b736eb46ae95a732f76e0be2b
Vinicius Cerqueira <viniciusleitecerqueira@gmail.com>
2025-07-19 16:34:05 -0300

    criados metodos get

--- src/main/kotlin/com/viniciusleitecerqueira/escola/business/AlunoBusiness.kt
+++ src/main/kotlin/com/viniciusleitecerqueira/escola/business/AlunoBusiness.kt
@@ -1,5 +1,15 @@
 package com.viniciusleitecerqueira.escola.business
 
+import com.viniciusleitecerqueira.escola.beans.Aluno
+import com.viniciusleitecerqueira.escola.repositories.AlunoRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -7,4 +17,19 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/alunos")
 class AlunoBusiness {
+
+    @Autowired
+    lateinit var alunoRepository: AlunoRepository
+
+    @GetMapping
+    fun listarTodos(): List<Aluno> = alunoRepository.findAll()
+
+
+    @GetMapping("/{id}")
+    fun buscarPorId(@PathVariable id: Long): ResponseEntity<Aluno> {
+        val aluno = alunoRepository.findById(id)
+        return if (aluno.isPresent) ResponseEntity.ok(aluno.get())
+        else ResponseEntity.notFound().build()
+    }
+
 }
