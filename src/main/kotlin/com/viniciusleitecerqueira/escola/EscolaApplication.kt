77224f49b4e7ac4f0973ca8241f3ff472a038de1
Vinicius Cerqueira <viniciusleitecerqueira@gmail.com>
2025-07-19 14:44:54 -0300

    commit inicial

---
+++ src/main/kotlin/com/viniciusleitecerqueira/escola/EscolaApplication.kt
@@ -0,0 +1,11 @@
+package com.viniciusleitecerqueira.escola
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EscolaApplication
+
+fun main(args: Array<String>) {
+	runApplication<EscolaApplication>(*args)
+}
